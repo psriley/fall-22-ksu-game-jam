dbd8aeeed9fcceebc43227a800c54eec394e2663
psriley <psriley@ksu.edu>
2022-10-15 11:46:31 -0500

    Add pathfinding

--- Fall22KSUGameJam/Assets/Patrol.cs
+++ Fall22KSUGameJam/Assets/Patrol.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
-    public float walkSpeed;
-    public float waitTime;
+    #region Basic Patrol
+    /*public float walkSpeed;
+    private float waitTime;
+    public float startWaitTime;
 
     public Transform[] patrolPoints;
     private int randomPoint;
 
     private void Start()
     {
+        waitTime = startWaitTime;
         randomPoint = Random.Range(0, patrolPoints.Length);
     }
 
@@ -21,13 +24,54 @@ public class Patrol : MonoBehaviour
 
         if (Vector2.Distance(transform.position, patrolPoints[randomPoint].position) < 0.2f)
         {
-            StartCoroutine("Wait");
+            if (waitTime <= 0)
+            {
+                randomPoint = Random.Range(0, patrolPoints.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+
         }
+    }*/
+    #endregion
+
+    #region Advanced Patrol
+    public float walkSpeed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform patrolPoint;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    private void Start()
+    {
+        waitTime = startWaitTime;
+
+        patrolPoint.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
     }
 
-    IEnumerator Wait()
+    private void Update()
     {
-        yield return new WaitForSeconds(waitTime);
-        randomPoint = Random.Range(0, patrolPoints.Length);
+        transform.position = Vector2.MoveTowards(transform.position, patrolPoint.position, walkSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, patrolPoint.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                patrolPoint.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
     }
+    #endregion
 }
