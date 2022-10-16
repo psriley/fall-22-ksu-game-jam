a887b75d4d19e05714936bb26a692538417b18f6
Peyton Riley <peyton@canopyteam.org>
2022-10-16 02:07:36 -0500

    WIP Detection and menus

---
+++ Fall22KSUGameJam/Assets/Guard.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    public float distance;
+    public LineRenderer lineOfSight;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Physics2D.queriesStartInColliders = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.hasChanged) {
+            Debug.Log("Transform has changed!");
+            anim.SetBool("isWalking", true);
+        }
+        else {
+            anim.SetBool("isWalking", false);
+        }
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, -transform.up, distance);
+        if (hitInfo.collider != null) {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            lineOfSight.SetPosition(1, hitInfo.point);
+
+            if (hitInfo.collider.CompareTag("Player")){
+                Debug.Log("Found you!");
+            }
+        }
+        else {
+            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+            lineOfSight.SetPosition(1, transform.position + transform.right * distance);
+        }
+
+        lineOfSight.SetPosition(0, transform.position);
+    }
+}
